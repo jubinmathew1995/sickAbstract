6a6fcf651a357374b2e7810bc9dc27208acc8b53
Jubin Mathew <jubinmathew1995@gmail.com>
2017-04-12 19:39:01 +0530

    added the primitive objects which is going to be used.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <GL/glut.h>
+#include <math.h>
 
 // window TITLE, WIDTH and HEIGHT
 #define WINDOW_TITLE_PREFIX "Object Puzzle"
 int windowWidth = 800,
     windowHeight = 600;
+
+// coodinates of the center for the primitives objects.
+int yPrimCoo = windowHeight-(0.15*windowHeight),i=0;
+int temp = windowWidth/7;
+int xPrimCoo[6] = {temp, 2*temp, 3*temp, 4*temp, 5*temp, 6*temp};
 
 // function prototype
 static void ResizeFunction(int, int);
@@ -46,6 +51,8 @@ int main(int argc, char *argv[])
     // sets the background color RED:GREEN:BLUE:ALPHA
     glClearColor(0,0,0,0);
 
+    gluOrtho2D(0,windowWidth,0,windowHeight);
+
     // enter GLUT event processing cycle/
     glutMainLoop();
 
@@ -62,17 +69,94 @@ static void ResizeFunction(int width, int height)
     gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
 }
+void drawCenterPointsForPrimitives ()
+{
+    glPointSize(3.0);	// sers the width of the point
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_POINTS);
+        glVertex3f(xPrimCoo[0],yPrimCoo,0.0);
+        glVertex3f(xPrimCoo[1],yPrimCoo,0.0);
+        glVertex3f(xPrimCoo[2],yPrimCoo,0.0);
+        glVertex3f(xPrimCoo[3],yPrimCoo,0.0);
+        glVertex3f(xPrimCoo[4],yPrimCoo,0.0);
+        glVertex3f(xPrimCoo[5],yPrimCoo,0.0);
+    glEnd();
+}
+void drawSquare(int x,int y)
+{
+    glColor3f(1.0,1.0,1.0);
+    int num=30;
+    glBegin(GL_POLYGON);
+        glVertex3f(x-num,y+num,0.0);
+        glVertex3f(x+num,y+num,0.0);
+        glVertex3f(x+num,y-num,0.0);
+        glVertex3f(x-num,y-num,0.0);
+    glEnd();
+}
+void drawRectangle(int x,int y)
+{
+    glColor3f(1.0,1.0,1.0);
+    int bre=30, len=50;
 
+    glBegin(GL_POLYGON);
+        glVertex3f(x-len,y+bre,0.0);
+        glVertex3f(x+len,y+bre,0.0);
+        glVertex3f(x+len,y-bre,0.0);
+        glVertex3f(x-len,y-bre,0.0);
+    glEnd();
+}
+void drawCircle(float cx, float cy, float r, int num_segments) {
+    glColor3f(1.0,1.0,1.0);
+
+    glBegin(GL_POLYGON);
+    for (int ii = 0; ii < num_segments; ii++)   {
+        float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
+        float x = r * cosf(theta);//calculate the x component
+        float y = r * sinf(theta);//calculate the y component
+        glVertex3f(x + cx, y + cy,0.0);//output vertex
+    }
+    glEnd();
+}
+void drawTriangle(int cx,int cy)
+{
+    glBegin(GL_TRIANGLES);
+        glVertex3f(cx,cy+30,0.0);
+        glVertex3f(cx-(30*0.8660), cy-(50.0*0.5), 0.0);
+        glVertex3f(cx+(30*0.8660), cy-(50.0*0.5), 0.0);
+    glEnd();
+}
+void drawHexagon(int x, int y)
+{
+    glBegin(GL_POLYGON);
+        glVertex3f(x-30,y,0.0);
+        glVertex3f(x-(30*0.5),y+(30*0.8660),0.0);
+        glVertex3f(x+(30*0.5),y+(30*0.8660),0.0);
+        glVertex3f(x+30,y,0.0);
+        glVertex3f(x+(30*0.5),y-(30*0.8660),0.0);
+        glVertex3f(x-(30*0.5),y-(30*0.8660),0.0);
+    glEnd();
+}
+void drawKite(int x,int y)
+{
+    glBegin(GL_POLYGON);
+        glVertex3f(x,y+30,0.0);
+        glVertex3f(x-30,y,0.0);
+        glVertex3f(x,y-50,0.0);
+        glVertex3f(x+30,y,0.0);
+    glEnd();
+}
 static void displayFunction(void)
 {
     // clear previous colors.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBegin(GL_TRIANGLES);
-        glVertex3f(-0.5,-0.5,0.0);
-        glVertex3f(0.5,0.0,0.0);
-        glVertex3f(0.0,0.5,0.0);
-    glEnd();
+    drawCenterPointsForPrimitives();
+    drawSquare(xPrimCoo[0],yPrimCoo);
+    drawRectangle(xPrimCoo[1],yPrimCoo);
+    drawCircle(xPrimCoo[2],yPrimCoo,30,100);
+    drawTriangle(xPrimCoo[3], yPrimCoo);
+    drawHexagon(xPrimCoo[4],yPrimCoo);
+    drawKite(xPrimCoo[5],yPrimCoo);
 
     // swaps the front and back buffers.
     glutSwapBuffers();
