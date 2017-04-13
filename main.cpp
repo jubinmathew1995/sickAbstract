f2675c2dce7422bdb44f64a5276ead93078e6e46
Jubin Mathew <jubinmathew1995@gmail.com>
2017-04-13 22:29:39 +0530

    Added code for moving objects using keyboard.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,24 @@ int windowWidth = 800,
 
 // coodinates of the center for the primitives objects.
 int yPrimCoo,temp,xPrimCoo[6];
+
+
+// movement global variables.
+struct movement
+{
+    int xMov=0,yMov=0;
+}mov[6];
+int flag=0;
+
+// function prototype
+static void ResizeFunction(int, int);
+static void displayFunction(void);
+static void idleFunction(void);
+static void keyFunction(unsigned char, int, int);
+static void mouseMove(int, int);
+static void mouseButton(int, int, int, int);
+
+// recalculate the centers for the objects.
 void calCenters()
 {
     yPrimCoo = windowHeight-(0.15*windowHeight);
@@ -91,16 +109,6 @@ public:
     }
 }topBar;
 
-// movement global variables.
-int xMov=0, yMov=0;
-
-// function prototype
-static void ResizeFunction(int, int);
-static void displayFunction(void);
-static void idleFunction(void);
-static void keyFunction(unsigned char, int, int);
-static void mouseMove(int, int);
-static void mouseButton(int, int, int, int);
 
 int main(int argc, char *argv[])
 {
@@ -180,23 +188,85 @@ void drawAxes()
     glVertex3f(windowWidth/2,0.0,0.0);
     glEnd();
 }
-void drawPoints()
-{
-    glPointSize(3.0);	// sers the width of the point
-    glColor3f(0.0,1.0,0.0);
-    glBegin(GL_POINTS);
-        glVertex3f((windowWidth*0.25)+(10.0*xMov), (windowHeight*0.35)+(10.0*yMov), 0.0);
-    glEnd();
-}
+// void drawPoints()
+// {
+//     glPointSize(3.0);	// sers the width of the point
+//     glColor3f(0.0,1.0,0.0);
+//     glBegin(GL_POINTS);
+//         glVertex3f((windowWidth*0.25)+(10.0*xMov), (windowHeight*0.35)+(10.0*yMov), 0.0);
+//     glEnd();
+// }
 void drawMovSquare()
 {
     glColor3f(1.0,1.0,1.0);
     int num=30;
     glBegin(GL_POLYGON);
-        glVertex3f((windowWidth*0.25)+(10.0*xMov)-num,(windowHeight*0.35)+(10.0*yMov)+num,0.0);
-        glVertex3f((windowWidth*0.25)+(10.0*xMov)+num,(windowHeight*0.35)+(10.0*yMov)+num,0.0);
-        glVertex3f((windowWidth*0.25)+(10.0*xMov)+num,(windowHeight*0.35)+(10.0*yMov)-num,0.0);
-        glVertex3f((windowWidth*0.25)+(10.0*xMov)-num,(windowHeight*0.35)+(10.0*yMov)-num,0.0);
+        glVertex3f((windowWidth*0.25)+(10.0*mov[0].xMov)-num,(windowHeight*0.35)+(10.0*mov[0].yMov)+num,0.0);
+        glVertex3f((windowWidth*0.25)+(10.0*mov[0].xMov)+num,(windowHeight*0.35)+(10.0*mov[0].yMov)+num,0.0);
+        glVertex3f((windowWidth*0.25)+(10.0*mov[0].xMov)+num,(windowHeight*0.35)+(10.0*mov[0].yMov)-num,0.0);
+        glVertex3f((windowWidth*0.25)+(10.0*mov[0].xMov)-num,(windowHeight*0.35)+(10.0*mov[0].yMov)-num,0.0);
+    glEnd();
+}
+void drawMovRectangle()
+{
+    glColor3f(1.0,1.0,1.0);
+    int bre=30, len=50;
+    float x = (windowWidth*0.25)+(10.0*mov[1].xMov),
+            y = (windowHeight*0.35)+(10.0*mov[1].yMov);
+    glBegin(GL_POLYGON);
+        glVertex3f(x-len,y+bre,0.0);
+        glVertex3f(x+len,y+bre,0.0);
+        glVertex3f(x+len,y-bre,0.0);
+        glVertex3f(x-len,y-bre,0.0);
+    glEnd();
+}
+void drawMovCircle() {
+    float num_segments=300,r=30,
+            cx = (windowWidth*0.25)+(10.0*mov[2].xMov),
+            cy = (windowHeight*0.35)+(10.0*mov[2].yMov);
+    glColor3f(1.0,1.0,1.0);
+
+    glBegin(GL_POLYGON);
+    for (int ii = 0; ii < num_segments; ii++)   {
+        float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
+        float x = r * cosf(theta);//calculate the x component
+        float y = r * sinf(theta);//calculate the y component
+        glVertex3f(x + cx, y + cy,0.0);//output vertex
+    }
+    glEnd();
+}
+void drawMovTriangle()
+{
+    int cx = (windowWidth*0.25)+(10.0*mov[3].xMov),
+        cy = (windowHeight*0.35)+(10.0*mov[3].yMov);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(cx,cy+30,0.0);
+        glVertex3f(cx-(30*0.8660), cy-(50.0*0.5), 0.0);
+        glVertex3f(cx+(30*0.8660), cy-(50.0*0.5), 0.0);
+    glEnd();
+}
+void drawMovHexagon()
+{
+    int x = (windowWidth*0.25)+(10.0*mov[4].xMov),
+        y = (windowHeight*0.35)+(10.0*mov[4].yMov);
+    glBegin(GL_POLYGON);
+        glVertex3f(x-30,y,0.0);
+        glVertex3f(x-(30*0.5),y+(30*0.8660),0.0);
+        glVertex3f(x+(30*0.5),y+(30*0.8660),0.0);
+        glVertex3f(x+30,y,0.0);
+        glVertex3f(x+(30*0.5),y-(30*0.8660),0.0);
+        glVertex3f(x-(30*0.5),y-(30*0.8660),0.0);
+    glEnd();
+}
+void drawMovKite()
+{
+    int x = (windowWidth*0.25)+(10.0*mov[5].xMov),
+        y = (windowHeight*0.35)+(10.0*mov[5].yMov);
+    glBegin(GL_POLYGON);
+        glVertex3f(x,y+30,0.0);
+        glVertex3f(x-30,y,0.0);
+        glVertex3f(x,y-50,0.0);
+        glVertex3f(x+30,y,0.0);
     glEnd();
 }
 static void displayFunction(void)
@@ -212,8 +282,14 @@ static void displayFunction(void)
     topBar.drawHexagon(xPrimCoo[4],yPrimCoo);
     topBar.drawKite(xPrimCoo[5],yPrimCoo);
     drawAxes();
-    drawPoints();
-    //drawMovSquare();
+//    drawPoints();
+    drawMovSquare();
+    drawMovRectangle();
+    drawMovCircle();
+    drawMovTriangle();
+    drawMovHexagon();
+    drawMovKite();
+
     // swaps the front and back buffers.
     glutSwapBuffers();
 }
@@ -239,21 +315,26 @@ static void keyFunction(unsigned char key, int x, int y)
             break;
         case 'W':
         case 'w':
-            yMov++;
+            mov[flag].yMov++;
             break;
         case 'S':
         case 's':
-            yMov--;
+            mov[flag].yMov--;
             break;
         case 'A':
         case 'a':
-            xMov--;
+            mov[flag].xMov--;
             break;
         case 'D':
         case 'd':
-            xMov++;
+            mov[flag].xMov++;
             break;
-
+        case '1':flag=0;break;
+        case '2':flag=1;break;
+        case '3':flag=2;break;
+        case '4':flag=3;break;
+        case '5':flag=4;break;
+        case '6':flag=5;break;
     }
     glutPostRedisplay();
 }
